095d905fb75c78ad34b50de78a44b18cb730fdc3
Mario <mario.perez094@gmail.com>
2025-05-17 07:23:03 -0700

    initial useGameState

---
+++ src/composables/useGameState.tsx
@@ -0,0 +1,129 @@
+import { createContext, useContext, useReducer } from "react";
+
+type GameStateType = {
+  phase: 
+    | "intro" 
+    | "wager" 
+    | "player-turn" 
+    | "daimon-turn" 
+    | "resolution" 
+    | "player-won" 
+    | "player-lost" 
+    | "intermission"; 
+  gameOver: boolean;
+  round: number;
+  handCount: number;
+  //Blood pool info
+  playerHealth: number,
+  playerMaxHealth: number,
+  daimonHealth: number,
+  daimonMaxHealth: number,
+  wager: number,
+  //Card info
+  playerHand: CardType[];
+  daimonHand: CardType[];
+  deck: CardType[];
+  discardPile: CardType[];
+  //Component visibility for cenamatic effect
+  visibility: VisibilityType
+};
+
+type GameActionType = 
+  | { type: "START_ROUND", payload: {
+      phase: string,
+      round: number,
+      handCount: number,
+      playerMaxHealth: number,
+      daimonHealth: number,
+      daimonMaxHealth: number,
+      wager: number,
+      playerHand: CardType[],
+      daimonHand: CardType[],
+      deck: CardType[],
+    }}
+  | { type: "SET_WAGER", payload: { wager: number } };
+
+type VisibilityType = {
+  daimon: boolean;
+  table: boolean;
+  healthBars: boolean;
+  dialogueBox: boolean;
+};
+
+const initialGameState: GameStateType = {
+  phase: "intro",
+  gameOver: false,
+  round: 0,
+  handCount: 0,
+  playerHealth: 5000,
+  playerMaxHealth: 5000,
+  daimonHealth: 2500,
+  daimonMaxHealth: 2500,
+  wager: 0,
+  playerHand: [],
+  daimonHand: [],
+  deck: [],
+  discardPile: [],
+  visibility: {
+    daimon: false,
+    table: false,
+    healthBars: false,
+    dialogueBox: false
+  }
+};
+
+function gameReducer(state: GameStateType, action: GameActionType): GameStateType {
+  switch(action.type) {
+    case "START_ROUND":
+      return { 
+        ...state,
+        phase: "wager",
+        round: state.round + 1,
+        handCount: state.handCount + 1,
+        playerMaxHealth: action.payload.playerMaxHealth,
+        daimonHealth: action.payload.daimonHealth,
+        daimonMaxHealth: action.payload.daimonMaxHealth,
+        wager: action.payload.wager,
+      };
+    default:
+      return state;
+  };
+};
+
+const GameContext = createContext<{
+  gameState: GameStateType;
+  gameDispatch: React.Dispatch<any>;
+} | null>(null);
+
+function GameProvider({ children } : { children: React.ReactNode }) {
+  const [gameState, gameDispatch] = useReducer(gameReducer, initialGameState);
+
+  return (
+    <GameContext.Provider value={{ gameState, gameDispatch }}>
+      { children }
+    </GameContext.Provider>
+  )
+};
+
+function useGameState() {
+  const context = useContext(GameContext);
+  if(!context) {
+    throw new Error("useGameState must be used within a GameProvider");
+  }
+  return context;
+};
+
+export { GameProvider, useGameState };
+
+export type SuitType = "Hearts" | "Diamonds" | "Clubs" | "Spades" | "None";
+
+export interface CardType {
+  name: string;
+  suit: SuitType;
+  value: number;
+  effectType: EffectType;
+  effect: number;
+};
+
+export type EffectType = "Blessed" | "Bloodstained" | "Charred" | "Exhumed" | "Fleshwoven" | "Standard";
+
